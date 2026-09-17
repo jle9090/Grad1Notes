75d18498e380615f4c3bca2ccc8407f1b78057aa
Justin Le <jle9090@github.com>
2026-09-16 20:49:11 -0600

    created skyplot for NIST

--- GPS/Homework/MATLAB/HW2_Part1_SCRIPT.m
+++ GPS/Homework/MATLAB/HW2_Part1_SCRIPT.m
@@ -7,18 +7,24 @@ clc; clear; close all
 % Reading in the sp3 files
 sp3 = read_sp3('IGS0OPSFIN_20262310000_01D_15M_ORB.SP3');
 
+% Extract every PRN up front and store in a struct array for ease of use.
+for prn_number = 1:32
+    [week_number, TOW_s, TOW_hr, time_epoch_s, time_epoch_hr, X_m, Y_m, Z_m, clock_bias_us, constellation, PRN] = extract_sp3_prn(sp3, prn_number);
+    satPRN(prn_number) = struct('week_number', week_number, 'TOW_s', TOW_s, 'TOW_hr', TOW_hr, ...
+        'time_epoch_s', time_epoch_s, 'time_epoch_hr', time_epoch_hr, 'X_m', X_m, 'Y_m', Y_m, 'Z_m', Z_m, ...
+        'clock_bias_us', clock_bias_us, 'constellation', constellation, 'PRN', PRN);
+end
+
 % Graphing PRN X,Y,Z [m] against time (0-24 hours)
 
 % a.
-% Select data for PRN5 from SP3
-[week_number_PRN5, TOW_s_PRN5, TOW_hr_PRN5, time_epoch_s_PRN5, time_epoch_hr_PRN5, X_m_PRN5, Y_m_PRN5, Z_m_PRN5, clock_bias_us_PRN5, constellation_PRN5] = extract_sp3_prn(sp3, 5);
-
+% Select data for PRN5 directly from satPRN
 figure()
-plot(time_epoch_hr_PRN5,X_m_PRN5,'LineWidth',1.5)
+plot(satPRN(5).time_epoch_hr,satPRN(5).X_m,'LineWidth',1.5)
 hold on
-plot(time_epoch_hr_PRN5,Y_m_PRN5,'LineWidth',1.5)
+plot(satPRN(5).time_epoch_hr,satPRN(5).Y_m,'LineWidth',1.5)
 hold on
-plot(time_epoch_hr_PRN5,Z_m_PRN5,'LineWidth',1.5)
+plot(satPRN(5).time_epoch_hr,satPRN(5).Z_m,'LineWidth',1.5)
 title('PRN05 X,Y,Z Over time')
 xlabel('Time [hrs]')
 ylabel('Distance [m]')
@@ -29,15 +35,15 @@ hold on
 [gps_ephem,gps_ephem_cell] = read_GPSyuma('YUMA231.alm.txt',2);
 
 
-[health_PRN5,satPos_m_PRN5,satClkCorr_PRN5] = alm2pos(gps_ephem,[week_number_PRN5, TOW_s_PRN5] ,5);
+[health_PRN5,satPos_m_PRN5,satClkCorr_PRN5] = alm2pos(gps_ephem,[satPRN(5).week_number, satPRN(5).TOW_s] ,5);
 
-plot(TOW_s_PRN5, satPos_m_PRN5, 'LineWidth',1.5)
+plot(satPRN(5).TOW_s, satPos_m_PRN5, 'LineWidth',1.5)
 legend('X SP3','Y SP3','Z SP3', 'X YUMA','Y YUMA','Z YUMA')
 
 % c. Plot residuals
-residuals_PRN5 = [satPos_m_PRN5(:,1)-X_m_PRN5,satPos_m_PRN5(:,2)-Y_m_PRN5,satPos_m_PRN5(:,3)-Z_m_PRN5];
+residuals_PRN5 = [satPos_m_PRN5(:,1)-satPRN(5).X_m,satPos_m_PRN5(:,2)-satPRN(5).Y_m,satPos_m_PRN5(:,3)-satPRN(5).Z_m];
 figure()
-plot(time_epoch_hr_PRN5,residuals_PRN5)
+plot(satPRN(5).time_epoch_hr,residuals_PRN5, 'LineWidth', 1.5)
 title('Difference Between SP3 and Almanac Results')
 xlabel('Time [hr]')
 ylabel('Difference [m]')
@@ -47,10 +53,5 @@ grid on
 % TODO: Describe: yeah its stll pretty bad to be off by an order of 1000
 % meters
 
-% Export for PRN8 for part 3 as well
-[week_number_PRN8, TOW_s_PRN8, TOW_hr_PRN8, time_epoch_s_PRN8, time_epoch_hr_PRN8, X_m_PRN8, Y_m_PRN8, Z_m_PRN8, clock_bias_us_PRN8, constellation_PRN8] = extract_sp3_prn(sp3, 8);
-
 % Export this for part 3.
-X_PRN5 = [X_m_PRN5,Y_m_PRN5,Z_m_PRN5];
-X_PRN8 = [X_m_PRN8,Y_m_PRN8,Z_m_PRN8];
-save("HW2Part1.mat", 'X_PRN5','TOW_s_PRN5', 'X_PRN5','TOW_s_PRN8', 'X_PRN8')
+save("HW2Part1.mat", 'satPRN')
--- GPS/Homework/MATLAB/extract_sp3_prn.m
+++ GPS/Homework/MATLAB/extract_sp3_prn.m
@@ -1,5 +1,5 @@
-function [week_number_prn, TOW_s_prn, TOW_hr_prn, time_epoch_s_prn, time_epoch_hr_prn, X_m_prn, Y_m_prn, Z_m_prn, clock_bias_us_prn, constellation_prn] = extract_sp3_prn(sp3, prn)
-% Extract and filter SP3 data for a single PRN.
+function [week_number_prn, TOW_s_prn, TOW_hr_prn, time_epoch_s_prn, time_epoch_hr_prn, X_m_prn, Y_m_prn, Z_m_prn, clock_bias_us_prn, constellation_prn, PRN_prn] = extract_sp3_prn(sp3, prn)
+% Extract and filter SP3 data for any single PRN.
 week_number = sp3(:,1);
 TOW_s = sp3(:,2);
 PRN = sp3(:,3);
@@ -10,17 +10,18 @@ clock_bias_us = sp3(:,7);
 constellation = sp3(:,8);
 time_epoch_s = TOW_s;
 
-prn_id = PRN == prn;
+prn_index = PRN == prn;
 
-week_number_prn = week_number(prn_id);
-TOW_s_prn = TOW_s(prn_id);
+week_number_prn = week_number(prn_index);
+TOW_s_prn = TOW_s(prn_index);
 TOW_hr_prn = TOW_s_prn/3600;
-time_epoch_s_prn = time_epoch_s(prn_id);
+time_epoch_s_prn = time_epoch_s(prn_index);
 time_epoch_hr_prn = time_epoch_s_prn;
-X_m_prn = X_m(prn_id);
-Y_m_prn = Y_m(prn_id);
-Z_m_prn = Z_m(prn_id);
-clock_bias_us_prn = clock_bias_us(prn_id);
-constellation_prn = constellation(prn_id);
+X_m_prn = X_m(prn_index);
+Y_m_prn = Y_m(prn_index);
+Z_m_prn = Z_m(prn_index);
+clock_bias_us_prn = clock_bias_us(prn_index);
+constellation_prn = constellation(prn_index);
+PRN_prn = PRN(prn_index);
 
 end
